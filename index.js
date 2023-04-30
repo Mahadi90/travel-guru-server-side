d5c18c50e688ee0912637e209bb5bb1d69326c9f
Mahadi90 <mahadimhs787@gmail.com>
2023-04-30 15:34:03 +0600

    id wise data added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+
 const places = require('./data/dada.json')
 
 app.get('/', (req, res) => {
@@ -9,6 +13,12 @@ app.get('/', (req, res) => {
 
 app.get('/places', (req, res) => {
     res.send(places)
+})
+
+app.get('/places/:id', (req , res) => {
+    const id  = req.params.id;
+    const selectedPlace = places.find(p => p.id === id);
+    res.send(selectedPlace)
 })
 app.listen(port, () => {
     console.log(`Travel guru API is running on port ${port}`)
