31321e2db18ee164a21f8e9847b87ac4d166d8f1
Mahadi90 <mahadimhs787@gmail.com>
2023-04-30 14:08:59 +0600

    places data added

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const places = require('./data/dada.json')
 
 app.get('/', (req, res) => {
     res.send('Travel guru is running')
 })
 
+app.get('/places', (req, res) => {
+    res.send(places)
+})
 app.listen(port, () => {
     console.log(`Travel guru API is running on port ${port}`)
 })
