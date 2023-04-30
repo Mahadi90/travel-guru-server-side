5e6e9fcf3e6b7ddc84c233e9d019ef1ec87876b3
Mahadi90 <mahadimhs787@gmail.com>
2023-05-01 00:21:59 +0600

    details data added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const places = require('./data/dada.json')
+const details = require('./data/deatils.json')
 
 app.get('/', (req, res) => {
     res.send('Travel guru is running')
@@ -19,6 +20,10 @@ app.get('/places/:id', (req , res) => {
     const id  = req.params.id;
     const selectedPlace = places.find(p => p.id === id);
     res.send(selectedPlace)
+})
+
+app.get('/details', (req, res) => {
+    res.send(details)
 })
 app.listen(port, () => {
     console.log(`Travel guru API is running on port ${port}`)
